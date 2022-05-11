e7cc9f89ed96529f9b7b74c303219e84fe5a817e
M.YasserAbdelkader <M.YasserAbdelkaderm.abdelkader.yasser@gmail.com>
2022-05-11 19:21:12 +0200

    Add Functions description

--- 02-MCAL/01-RCC/RCC_interface.h
+++ 02-MCAL/01-RCC/RCC_interface.h
@@ -14,8 +14,32 @@
 #define RCC_APB1    1
 #define RCC_APB2    2
 
+/************************************************************************************************
+ * \fn      RCC_voidInitSysClock
+ * \brief   Initiate System Clock
+************************************************************************************************/
 void RCC_voidInitSysClock(void);
+
+/************************************************************************************************
+ * \fn      RCC_voidEnableClock
+ * \brief   Enable peripheral Clock
+ * \param   copy_u8BusId    u8 Bus ID. it can have the following values
+ * \li      \c RCC_AHB      AHB Bus
+ * \li      \c RCC_APB1     APB1 Bus
+ * \li      \c RCC_APB2     APB2 Bus
+ * \param   copy_u8PerID    u8 Peripheral ID. it takes values from 0 - 31. 
+************************************************************************************************/
 void RCC_voidEnableClock(u8 copy_u8BusId, u8 copy_u8PerID);
+
+/************************************************************************************************
+ * \fn      RCC_voideDisableClock
+ * \brief   Disable peripherals Clock
+ * \param   copy_u8BusId    u8 Bus ID. it can have the following values
+ * \li      \c RCC_AHB      AHB Bus
+ * \li      \c RCC_APB1     APB1 Bus
+ * \li      \c RCC_APB2     APB2 Bus
+ * \param   copy_u8PerID    u8 Peripheral ID. it takes values from 0 - 31. 
+************************************************************************************************/
 void RCC_voideDisableClock(u8 copy_u8BusId, u8 copy_u8PerID);
 
 #endif
