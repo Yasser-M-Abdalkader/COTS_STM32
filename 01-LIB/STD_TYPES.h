aa59eaa083f2adc1f9b9677c174d9739b5cd1d82
M.YasserAbdelkader <M.YasserAbdelkaderm.abdelkader.yasser@gmail.com>
2022-05-12 06:44:43 +0200

    Minor Changes

--- 01-LIB/STD_TYPES.h
+++ 01-LIB/STD_TYPES.h
@@ -3,7 +3,7 @@
  * @brief   STD Types - LIB
  * @version 0.1
  * @date 2022-03-10
- * @copyright Copyright (c) 2022 
+ * @copyright Copyright (c) 2022
  *************************************************************/
 
 /** \headerfile STD_TYPES.h "STD_TYPES.h"*/
@@ -15,18 +15,20 @@
  * \brief    defines standard types
  * @{
  */
-typedef unsigned char               u8;
-typedef unsigned short int          u16;
-typedef unsigned long  int          u32;
+typedef unsigned char           u8;
+typedef unsigned short int      u16;
+typedef unsigned long int       u32;
 
-typedef signed char                 s8;
-typedef signed short int            s16;
-typedef signed long  int            s32;
+typedef signed char             s8;
+typedef signed short int        s16;
+typedef signed long int         s32;
 
-typedef float                       f32;
-typedef double                      f64;
+typedef float                   f32;
+typedef double                  f64;
 /** @}
- * 
-*************************************************************/
+ *
+ *************************************************************/
+
+#define NULL                    void * 0
 
 #endif
--- 02-MCAL/2-GPIO/GPIO_interface.h
+++ 02-MCAL/2-GPIO/GPIO_interface.h
@@ -8,56 +8,64 @@
  ***********************************************************************************/
 #ifndef GPIO_INTERFACE_H
 #define GPIO_INTERFACE_H
+/************************************************************************************/
+#define GPIO_HIGH                       1   /* GPIO OutPut High                     */
+#define GPIO_LOW                        0   /* GPIO OutPut Low                      */
+/************************************************************************************/
+#define GPIOA                           0   /* GPIO Port A                          */
+#define GPIOB                           1   /* GPIO Port B                          */
+#define GPIOC                           2   /* GPIO Port C                          */
+/************************************************************************************/
+#define GPIO_PIN0                       0   /* GPIO Pin 0                           */
+#define GPIO_PIN1                       1   /* GPIO Pin 1                           */
+#define GPIO_PIN2                       2   /* GPIO Pin 2                           */
+#define GPIO_PIN3                       3   /* GPIO Pin 3                           */
+#define GPIO_PIN4                       4   /* GPIO Pin 4                           */
+#define GPIO_PIN5                       5   /* GPIO Pin 5                           */
+#define GPIO_PIN6                       6   /* GPIO Pin 6                           */
+#define GPIO_PIN7                       7   /* GPIO Pin 7                           */
+#define GPIO_PIN8                       8   /* GPIO Pin 8                           */
+#define GPIO_PIN9                       9   /* GPIO Pin 9                           */
+#define GPIO_PIN10                      10  /* GPIO Pin 10                          */
+#define GPIO_PIN11                      11  /* GPIO Pin 11                          */
+#define GPIO_PIN12                      12  /* GPIO Pin 12                          */
+#define GPIO_PIN13                      13  /* GPIO Pin 13                          */
+#define GPIO_PIN14                      14  /* GPIO Pin 14                          */
+#define GPIO_PIN15                      15  /* GPIO Pin 15                          */
+/************************************************************************************/
+/* Input Configuration modes                                                        */
+/************************************************************************************/
+#define GPIO_INPUT_ANALOG               0b0000 /* Input Analog                      */
+#define GPIO_INPUT_FLOATING             0b0100 /* Input floating                    */
+#define GPIO_INPUT_PULL_UP              0b1000 /* Input pull-down                   */
+#define GPIO_INPUT_PULL_DOWN            0b1000 /* Input pull-up                     */
+/************************************************************************************/
+/* Output Configuration modes                                                       */
+/************************************************************************************/
+/* Maximum output speed 10 MHz                                                      */
+#define GPIO_OUTPUT_SPEED_10MHZ_PP      0b0001 /* Output Push-pull                  */
+#define GPIO_OUTPUT_SPEED_10MHZ_OD      0b0101 /* Output Open-drain                 */
+#define GPIO_OUTPUT_SPEED_10MHZ_AFPP    0b1001 /* Alternate Function Push-pull      */
+#define GPIO_OUTPUT_SPEED_10MHZ_AFOD    0b1101 /* Alternate Function Open-drain     */
+/************************************************************************************/
+/* Maximum output speed 2 MHz                                                       */
+#define GPIO_OUTPUT_SPEED_2MHZ_PP       0b0011 /* Output Push-pull                  */
+#define GPIO_OUTPUT_SPEED_2MHZ_OD       0b0110 /* Output Open-drain                 */
+#define GPIO_OUTPUT_SPEED_2MHZ_AFPP     0b1010 /* Alternate Function Push-pull      */
+#define GPIO_OUTPUT_SPEED_2MHZ_AFOD     0b1110 /* Alternate Function Open-drain     */
+/************************************************************************************/
+/* Maximum output speed 50 MHz                                                      */
+#define GPIO_OUTPUT_SPEED_50MHZ_PP      0b0011 /* Output Push-pull                  */
+#define GPIO_OUTPUT_SPEED_50MHZ_OD      0b0111 /* Output Open-drain                 */
+#define GPIO_OUTPUT_SPEED_50MHZ_AFPP    0b1011 /* Alternate Function Push-pull      */
+#define GPIO_OUTPUT_SPEED_50MHZ_AFOD    0b1111 /* Alternate Function Open-drain     */
+/************************************************************************************/
 
-#define GPIO_HIGH       1
-#define GPIO_LOW        0
+u8 MGPIO_voidSetPinDirection(u8 copy_u8Port, u8 copy_u8Pin , u8 copy_u8Mode);
 
-#define GPIOA           0
-#define GPIOB           1
-#define GPIOC           2
+u8 MGPIO_voidSetPinValue(u8 copy_u8Port, u8 copy_u8Pin , u8 copy_u8Value);
 
-#define GPIO_PIN0            0
-#define GPIO_PIN1            1
-#define GPIO_PIN2            2
-#define GPIO_PIN3            3
-#define GPIO_PIN4            4
-#define GPIO_PIN5            5
-#define GPIO_PIN6            6
-#define GPIO_PIN7            7
-#define GPIO_PIN8            8
-#define GPIO_PIN9            9
-#define GPIO_PIN10           10
-#define GPIO_PIN11           11
-#define GPIO_PIN12           12
-#define GPIO_PIN13           13
-#define GPIO_PIN14           14
-#define GPIO_PIN15           15
+u8 MGPIO_u8GetPinValue(u8 copy_u8Port, u8 copy_u8Pin, u8* copy_pu8PinValue);
 
-#define GPIO_INPUT_ANALOG                0b0000
-#define GPIO_INPUT_FLOATING              0b0100
-#define GPIO_INPUT_PULL_UP               0b1000
-#define GPIO_INPUT_PULL_DOWN             0b1000
-
-
-#define GPIO_OUTPUT_SPEED_10MHZ_PP       0b0001
-#define GPIO_OUTPUT_SPEED_10MHZ_OD       0b0101
-#define GPIO_OUTPUT_SPEED_10MHZ_AFPP     0b1001
-#define GPIO_OUTPUT_SPEED_10MHZ_AFOD     0b1101
-
-#define GPIO_OUTPUT_SPEED_2MHZ_PP        0b0011
-#define GPIO_OUTPUT_SPEED_2MHZ_OD        0b0110
-#define GPIO_OUTPUT_SPEED_2MHZ_AFPP      0b1010
-#define GPIO_OUTPUT_SPEED_2MHZ_AFOD      0b1110
-
-#define GPIO_OUTPUT_SPEED_50MHZ_PP       0b0011
-#define GPIO_OUTPUT_SPEED_50MHZ_OD       0b0111
-#define GPIO_OUTPUT_SPEED_50MHZ_AFPP     0b1011
-#define GPIO_OUTPUT_SPEED_50MHZ_AFOD     0b1111
-
-
-void MGPIO_voidSetPinDirection(u8 copy_u8Port, u8 copy_u8Pin , u8 copy_u8Mode);
-
-void MGPIO_voidSetPinValue(u8 copy_u8Port, u8 copy_u8Pin , u8 copy_u8Value);
-
-u8 MGPIO_u8GetPinValue(u8 copy_u8Port, u8 copy_u8Pin);
+void MGPIO_voidLockPin(u8 copy_u8Port, u8 copy_u8Pin);
 #endif
